16420409da7dc35e361587f325418efe0a2c1199
nikitamankape <nikitamankape95@gmail.com>
2026-06-16 23:11:23 +0530

    reverted the updated program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -74,11 +74,11 @@ app.UseAuthorization();
 app.MapControllers();
 
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
-}
+//using (var scope = app.Services.CreateScope())
+//{
+//    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+//    db.Database.Migrate();
+//}
 
 
 app.Run();
